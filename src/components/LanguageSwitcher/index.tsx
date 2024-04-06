42730906c83caa2f620058e045677c17a400979b
Roger Faria2 <rfaria2@lenovo.com>
2024-04-06 00:10:58 -0300

    Setup App.Tsx and Pre Config i18n

---
+++ src/components/LanguageSwitcher/index.tsx
@@ -0,0 +1,25 @@
+import us from "./assets/us.svg";
+import fr from "./assets/fr.svg";
+import br from "./assets/br.svg";
+
+const languageOptions = [
+  {
+    name: "English",
+    value: "en",
+    flag: us,
+  },
+  {
+    name: "Português",
+    value: "ptBr",
+    flag: br,
+  },
+  {
+    name: "French",
+    value: "ptBr",
+    flag: fr,
+  },
+];
+
+export const LanguageSwitcher = () => {
+  return <></>;
+};
