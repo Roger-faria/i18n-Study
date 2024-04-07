3af475b7467970107156a24654d8d86741f01b91
Roger Faria2 <rfaria2@lenovo.com>
2024-04-07 19:06:54 -0300

    Added MuiButton and dinamic values to translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { useTranslation } from "react-i18next";
 import "./App.css";
 import { LanguageSwitcher } from "./components/LanguageSwitcher";
+import { useState } from "react";
+import { Button } from "@mui/material";
 
 function App() {
   const { t } = useTranslation();
+  const [counter, setCounter] = useState(0);
+  const addCounter = () => {
+    setCounter(counter + 1);
+  };
   return (
     <>
       <div className="App">
@@ -13,6 +19,10 @@ function App() {
           <h2>{t("whereYouCan")}</h2>
         </header>
       </div>
+      <Button onClick={() => addCounter()} variant="contained" color="primary">
+        {t("button")}
+      </Button>
+      <h3>{t("selectedCounter", { counter: counter })}</h3>
     </>
   );
 }
